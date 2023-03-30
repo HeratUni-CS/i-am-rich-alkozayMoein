ba1cefbcf869c3fe18250609cea39b789e5b5fdd
Moein Alkozay <moein.alkozay@microcis.net>
2023-03-30 06:21:29 +0430

    Adding some new widgets to the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[800],
-          title: Text('I am Moein'),
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[800],
+            title: Text('I am Moein'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'I am Moein Alkoay. I am Node.js developer at Microcis LLC. This is my very fisrt project with flutter. ',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 50.0),
+              Image(
+                image: AssetImage(
+                    'images/moein.jpg'
+                ),
+              ),
+            ],
+          ),
         ),
-        body: Center(
-          child: Image(
-            image: AssetImage(
-              'images/moein.jpg'
-            ),
-          )
-        ),
-      ),
-    )
+      )
   );
 }
-
-
